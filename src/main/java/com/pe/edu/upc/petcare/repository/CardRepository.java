8fd29b721093d3406222aef6d39d16416120b3b4
EgoPro1 <joaquinegocheagaramos@gmail.com>
2020-05-09 11:57:47 -0500

    metodo de pago

---
+++ src/main/java/com/pe/edu/upc/petcare/repository/CardRepository.java
@@ -0,0 +1,14 @@
+package com.pe.edu.upc.petcare.repository;
+
+
+import com.pe.edu.upc.petcare.model.Card;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CardRepository extends JpaRepository<Card,Long> {
+
+    //Card findByNumberId(Long id);
+
+
+}
