4999c5b4aea1de83bb5473af513a47b4811f3d11
Guillermo <aries_20_15@hotmail.com>
2020-06-05 23:32:37 -0500

    add review

--- src/main/java/com/pe/edu/upc/petcare/controller/ProvidersController.java
+++ src/main/java/com/pe/edu/upc/petcare/controller/ProvidersController.java
@@ -20,7 +20,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/business/{businessId}/providers")
-public class ProviderController {
+public class ProvidersController {
     @Autowired
     private ModelMapper mapper;
     @Autowired
--- src/main/java/com/pe/edu/upc/petcare/service/impl/ProductTypeServiceImpl.java
+++ src/main/java/com/pe/edu/upc/petcare/service/impl/ProductTypeServiceImpl.java
@@ -2,8 +2,8 @@ package com.pe.edu.upc.petcare.service.impl;
 
 import com.pe.edu.upc.petcare.exception.ResourceNotFoundException;
 import com.pe.edu.upc.petcare.model.ProductType;
-import com.pe.edu.upc.petcare.repository.TypeProductRepository;
-import com.pe.edu.upc.petcare.service.TypeProductService;
+import com.pe.edu.upc.petcare.repository.ProductTypeRepository;
+import com.pe.edu.upc.petcare.service.ProductTypeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,40 +11,40 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
-public class TypeProductServiceImpl implements TypeProductService {
+public class ProductTypeServiceImpl implements ProductTypeService {
     @Autowired
-    TypeProductRepository typeProductRepository;
+    ProductTypeRepository productTypeRepository;
 
     @Override
     public Page<ProductType> getAllServiceType(Pageable pageable) {
-        return typeProductRepository.findAll(pageable);
+        return productTypeRepository.findAll(pageable);
     }
 
     @Override
     public ProductType getServiceTypeById(Long serviceTypeId) {
-        return typeProductRepository.findById(serviceTypeId)
+        return productTypeRepository.findById(serviceTypeId)
                 .orElseThrow(()->new ResourceNotFoundException("ServiceType","Id",serviceTypeId));
     }
 
     @Override
     public ProductType createServiceType(ProductType productType) {
-        return typeProductRepository.save(productType);
+        return productTypeRepository.save(productType);
     }
 
     @Override
     public ProductType updateServiceType(Long serviceTypeId, ProductType productTypeRequest) {
-        ProductType productType = typeProductRepository.findById(serviceTypeId)
+        ProductType productType = productTypeRepository.findById(serviceTypeId)
                 .orElseThrow(()->new ResourceNotFoundException("ServiceType","Id",serviceTypeId));
         productType.setName(productTypeRequest.getName());
 
-        return typeProductRepository.save(productType);
+        return productTypeRepository.save(productType);
     }
 
     @Override
     public ResponseEntity<?> deleteServiceType(Long serviceTypeId) {
-        ProductType productType = typeProductRepository.findById(serviceTypeId)
+        ProductType productType = productTypeRepository.findById(serviceTypeId)
                 .orElseThrow(()->new ResourceNotFoundException("ServiceType","Id",serviceTypeId));
-        typeProductRepository.delete(productType);
+        productTypeRepository.delete(productType);
 
         return ResponseEntity.ok().build();
     }
